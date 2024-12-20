ce8625cf668fb8251675f370ccbb9ee188c936f9
ks-cap <ks.cap.main@gmail.com>
2024-12-20 21:31:09 +0900

    first commit

---
+++ App/App/App.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by 佐藤　賢 on 2024/12/20.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# GithubSampleApp

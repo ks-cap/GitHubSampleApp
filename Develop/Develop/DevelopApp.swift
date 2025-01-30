2de271f082fab23fdf7758baeea024a768c9160a
ks-cap <ks.cap.main@gmail.com>
2025-01-30 15:06:32 +0900

    Add Develop directory

---
+++ Develop/Develop/DevelopApp.swift
@@ -0,0 +1,12 @@
+import DevelopApp
+import SwiftUI
+
+@main
+struct DevelopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DevelopRootScreen()
+        }
+    }
+}
+
---
+++ Develop/DevelopTests/DevelopTests.swift
@@ -0,0 +1,16 @@
+//
+//  DevelopTests.swift
+//  DevelopTests
+//
+//  Created by 佐藤　賢 on 2025/01/30.
+//
+
+import Testing
+
+struct DevelopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
